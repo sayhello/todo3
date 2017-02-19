41f8723dbdf197cf39702de9d10ee6255263e2dd
sayhello <sayhello@safe-mail.net>
2017-02-19 13:29:10 +0100

    firebase add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,48 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
 
+
 // Import the ButtonsModule...
 import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { ListComponent } from './list/list.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'list', component: ListComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: "AIzaSyBqNs2sYjhXtr7cwiei3ZB0mhck7v6RW3U",
+  authDomain: "datemenow-764c3.firebaseapp.com",
+  databaseURL: "https://datemenow-764c3.firebaseio.com",
+  storageBucket: "datemenow-764c3.appspot.com",
+  messagingSenderId: "1048068159574"
+
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(firebaseConfig),
     ButtonsModule
   ],
   providers: [],
---
+++ src/app/list/list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import 'rxjs/Rx';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent implements OnInit {
+  items: FirebaseListObservable<any[]>;
+  constructor(af: AngularFire) {
+    this.items = af.database.list('', {
+      query: {
+        limitToLast: 4
+      }
+    }).map((array) => array.reverse()) as FirebaseListObservable<any[]>;
+  }
+
+  addItem(newName: string) {
+    this.items.push({ text: newName });
+  }
+
+  ngOnInit() {
+  }
+
+}
